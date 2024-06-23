806eb781d33bdced8ab58dc9b59eac1744396e1a
stuvloggram-edtech <you@example.com>
2024-06-23 14:23:45 +0530

    First commit

---
+++ controls.js
@@ -0,0 +1,23 @@
+class Controls{
+    constructor(){
+        this.forward = false;
+        this.left = false;
+        this.right = false;
+        this.reverse = false;
+
+        this.#addKeyboardListeners();
+    }
+
+    #addKeyboardListeners(){
+        document.onkeydown=(e) =>{
+            switch(e.key){
+                case "ArrowLeft":
+                    this.left=true
+                    break
+                case "ArrowRight":
+                    this.right=true
+                    break
+            }
+        }
+    }
+} 
