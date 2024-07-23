b9cd27be7baf3d3954d0701b5e35de637d51f269
Pinche14 <shubham70254@gmail.com>
2024-07-24 02:44:08 +0530

    Check 2

--- controls.js
+++ controls.js
@@ -9,15 +9,41 @@ class Controls{
     }
 
     #addKeyboardListeners(){
-        document.onkeydown=(e) =>{
+        document.onkeydown= (e) =>{
             switch(e.key){
                 case "ArrowLeft":
-                    this.left=true
+                    this.left= true;
+                    break;
+                case "ArrowRight":
+                    this.right= true;
+                    break;
+                case "ArrowUp" :
+                    this.forward = true;
+                    break;
+                case "ArrowDown":
+                    this.reverse = true;
+                    break;
+                   
+            }
+            console.table(this)
+        }
+        document.onkeyup= (e) =>{
+            switch(e.key){
+                case "ArrowLeft":
+                    this.left= false;
                     break
                 case "ArrowRight":
-                    this.right= true
-                    break
+                    this.right= false;
+                    break;
+                case "ArrowUp" :
+                    this.forward = false;
+                    break;
+                case "ArrowDown":
+                    this.reverse = false;
+                    break;
             }
+            console.table(this)
+        
         }
     }
 } 
